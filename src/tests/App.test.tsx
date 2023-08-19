984c1505a72ac61160d0202c1a8af2fdc1fb47d1
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-08-19 19:28:03 -0300

    requisito 5

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { PlanetProvider } from '../ContextApi/PlanetContext'; // Certifique-se de importar o provedor correto
+import FilterInterface from '../components/FilterInterface';
+import Table from '../components/Table';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('FilterInterface component', () => {
+  it('renders correctly', () => {
+    render(
+      <PlanetProvider>
+        <FilterInterface />
+      </PlanetProvider>
+    );
+
+
+    expect(screen.getByTestId('column-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('comparison-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('value-filter')).toBeInTheDocument();
+    expect(screen.getByTestId('button-filter')).toBeInTheDocument();
+  });
+
+  it('filters planets correctly', () => {
+
+  });
+});
+
+describe('Table component', () => {
+  it('renders correctly', () => {
+    // Mock dos dados de planetas
+    const planetsDataMock = [
+
+    ];
+
+    render(
+      <PlanetProvider>
+        <Table />
+      </PlanetProvider>
+    );
+
+
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Climate')).toBeInTheDocument();
+    expect(screen.getByText('Terrain')).toBeInTheDocument();
+
+  });
 });
